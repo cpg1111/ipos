ed9a20544f06aed1717bc7b0fd337820e154eefa
Christian Grabowski <christiang11754@gmail.com>
2016-11-29 19:31:54 -0500

    change tap constructor to pure Go

--- false_net/tap/tap.go
+++ false_net/tap/tap.go
@@ -1,10 +1,25 @@
 package tap
 
 import (
-	"net"
 	"os"
+
+	"github.com/cpg1111/ipos/false_net/netinterface"
 )
 
-func Alloc(dev string) (uintptr, error) {
+const (
+	IFF_TAP   = 0x0002
+	IFF_NO_PI = 0x1000
+)
 
+func New(name string) (intfce *netinterface.Interface, err error) {
+	file, err := os.OpenFile("/dev/net/tun", os.ORDWR, 0)
+	if err != nil {
+		return nil, err
+	}
+	iName, err := netinterface.NewReq(file.Fd(), name, IFF_TAP|IFF_NO_PI)
+	if err != nil {
+		return nil, err
+	}
+	intface = netinterface.New(file, true, iName)
+	return
 }
--- false_net/tap/tap.h
+++
@@ -1,4 +0,0 @@
-#ifndef TAP_H_
-#define TAP_H_
-    int tun_alloc(char* dev);
-#endif
